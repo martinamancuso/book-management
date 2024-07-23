6d07d5f22fbdba357ab4b6a0ebbfd5484c08bcd9
martinamancuso <martinamancuso24@gmail.com>
2024-07-23 10:34:22 +0200

    feat: create a reducer - change functions

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -4,4 +4,13 @@ import { Book } from '../models/book';
 
 export const initialState: ReadonlyArray<Book> = [];
 
-export const BookReducer = createReducer(initialState);
+export const BookReducer = createReducer(
+  initialState,
+  on(AddBook, (state, { id, title, author }) => [
+    ...state,
+    { id, title, author },
+  ]),
+  on(RemoveBook, (state, { bookId }) =>
+    state.filter((book) => book.id !== bookId)
+  )
+);
