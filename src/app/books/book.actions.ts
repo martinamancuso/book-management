c5469d97f61814eb240c10e7d691cf323a23d253
martinamancuso <martinamancuso24@gmail.com>
2024-07-26 10:41:51 +0200

    feat: create success and failure actions

--- src/app/books/book.actions.ts
+++ src/app/books/book.actions.ts
@@ -3,6 +3,14 @@ import { Book } from '../models/book';
 
 // first action
 export const AddBook = createAction('[Book] Add Book', props<Book>());
+export const AddBookSuccess = createAction(
+  '[Book] Add Book Successfully',
+  props<Book>()
+);
+export const AddBookFailure = createAction(
+  '[Book] Add Book Failure',
+  props<{ error: any }>()
+);
 
 // second action
 export const RemoveBook = createAction(
